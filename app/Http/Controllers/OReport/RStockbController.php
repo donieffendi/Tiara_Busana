17553dffdcfdb75818a00eb16769bc9c0b02bf28
ifaindrianningsih <ifaindrian152@gmail.com>
2026-03-25 16:19:09 +0800

    update sales discount counter

--- app/Http/Controllers/OReport/RStockbController.php
+++ app/Http/Controllers/OReport/RStockbController.php
@@ -35,9 +35,6 @@ class RStockbController extends Controller
 	
 	public function jasperStockbReport(Request $request) 
 	{
-		$file 	= 'stockn';
-		$PHPJasperXML = new PHPJasperXML();
-		$PHPJasperXML->load_xml_file(base_path().('/app/reportc01/phpjasperxml/'.$file.'.jrxml'));
 		
 			// Ganti format tanggal input agar sama dengan database
 			$tglDrD = date("Y-m-d", strtotime($request['tglDr']));
@@ -61,7 +58,9 @@ class RStockbController extends Controller
 			session()->put('filter_semua', $request->semua);
 		
 		if ($semua == 1){
-			$query = DB::SELECT("SELECT AA.cnt, AA.ncnt,'' as kd_brg,'' as na_brg, SUM(AA.qtyx) as qtyx, round(SUM(AA.total_cash)) as total_cash, AA.dis,       
+			
+			$file 	= 'disk conter all';
+			$query = DB::SELECT("SELECT AA.cnt as cnt, AA.ncnt as ncnt,'' as kd_brg,'' as na_brg, SUM(AA.qtyx) as qtyx, round(SUM(AA.total_cash)) as total_cash, AA.dis,       
 								AA.par, SUM(AA.beban_cash) as beban_cash, AA.tgl, AA.no_bukti,IF(YY.CNT IS NULL,0,1) AS KONDISI                            
 								FROM                                                                   
 								(SELECT disbsn.NO_BUKTI,brgbsn.CNT,	brgbsn.NCNT,                                  
@@ -128,7 +127,10 @@ class RStockbController extends Controller
 										AND brgbsn.CNT>='$kode1'  AND brgbsn.CNT<='$kode2' AND brgbsn.CNT=disbsn.CNT AND disbsn.TYPE='ALL' AND disbsn.TGLM=disbsn.TGLS  and disbsn.DIS<>0    AND DISBSN.TGLS>='$tglDrD' AND DISBSN.TGLM<='$tglSmpD'                         
 										GROUP BY brgbsn.CNT, DATE(juald$bulan.TGL),disbsn.NO_BUKTI                            
 								) as AA  GROUP BY AA.CNT, AA.TGL, AA.NO_BUKTI  ORDER BY CNT,TGL");
+	
 		} else {
+			
+			$file 	= 'disk conter';
 			$query = DB::SELECT("SELECT AA.cnt, AA.ncnt, AA.kd_brg,AA.na_brg, SUM(AA.qtyx) as qtyx, round(SUM(AA.total_cash)) as total_cash, AA.dis,                    
 								AA.par, SUM(AA.beban_cash) as beban_cash, AA.tgl, AA.no_bukti,IF(YY.KD_BRG IS NULL,0,1) AS KONDISI                                    
 								FROM                                                                                
@@ -206,19 +208,14 @@ class RStockbController extends Controller
 			return view('oreport_stockb.report')->with(['per' => $per])->with(['hasil' => $query]);
 		}
 
-		$data=[];
-		foreach ($query as $key => $value)
-		{
-			array_push($data, array(
-				'NO_BUKTI' => $query[$key]->NO_BUKTI,
-				'TGL' => $query[$key]->TGL,
-				'KD_BRG' => $query[$key]->KD_BRG,
-				'NA_BRG' => $query[$key]->NA_BRG,
-				'KG' => $query[$key]->KG,
-				'NOTES' => $query[$key]->NOTES,
-			));
-		}
-		$PHPJasperXML->setData($data);
+		$PHPJasperXML = new PHPJasperXML();
+		$PHPJasperXML->load_xml_file(base_path().('/app/reportc01/phpjasperxml/'.$file.'.jrxml'));
+		
+		$cleanData = json_decode(json_encode($query), true);
+       	$PHPJasperXML->setData($cleanData);
+		$PHPJasperXML->arrayParameter = [
+			"TGL_CTK" => date('d/m/Y')
+        ];
 		ob_end_clean();
 		$PHPJasperXML->outpage("I");
 	}
