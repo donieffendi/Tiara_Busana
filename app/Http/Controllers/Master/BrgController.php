7a8b189daaa7410c7a6a328cc3c0676a5db03c4c
Doni Effendi <doni.effendi.2014@gmail.com>
2026-04-12 09:07:35 +0700

    cek session cabang di master barang

--- app/Http/Controllers/Master/BrgController.php
+++ app/Http/Controllers/Master/BrgController.php
@@ -68,6 +68,7 @@ class BrgController extends Controller
 		// $PPN = Auth::user()->PPN;
 		
         $dept = session()->get('periode')['dept'];
+        $cabang = session()->get('periode')['cabang'];
 
 
         $brg = DB::SELECT("SELECT NO_ID, SUB, KDBAR, NMBAR, ITEM_SUP, KDBAR, KET_UK, KET_KEM, SUPP, QTY_BELI1, HB, DIS_A, DIS_B, DIS_C, TOT_BL 
