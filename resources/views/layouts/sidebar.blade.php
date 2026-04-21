a3fd2d84c010ce7538467987610b63fcc21fc720
Nadiah Elvira Syahputri <nadiahelvira56@gmail.com>
2026-04-21 14:45:28 +0700

    xx

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -742,7 +742,7 @@
 
         <div class="col-md-3">
             <div class="menu-card" style="border:1px solid #aabbcc; background-color:#ffe0ba;">
-                <a href="javascript:addTab('Usulan Perubahan Komponen Harga', '{{url('ubbrgdw?flagz=KZ')}}')">
+                <a href="javascript:addTab('Usulan Perubahan Komponen Harga', '{{url('ubbrgdw')}}')">
                   <!-- <i class="nav-icon fas fa-anchor icon-blue" style="text-align: center;"></i> -->
                   <i style="margin-left:-5px;font-size: 40px;" class="nav-icon fas fa-retweet icon-orange"></i>
                   <h6>Usulan Perubahan</h6>
@@ -753,7 +753,7 @@
 
         <div class="col-md-3">
             <div class="menu-card" style="border:1px solid #aabbcc; background-color:#ffe0ba;">
-                <a href="javascript:addTab('Usulan Penghapusan Harga Komponen', '{{url('ubbrgdw?flagz=KZ')}}')">
+                <a href="javascript:addTab('Usulan Penghapusan Harga Komponen', '{{url('ubbrgdw')}}')">
                   <!-- <i class="nav-icon fas fa-anchor icon-blue" style="text-align: center;"></i> -->
                   <i style="margin-left:-5px;font-size: 40px;" class="nav-icon fas fa-retweet icon-orange"></i>
                   <h6>Usulan Penghapusan</h6>
