2f8550fc4c8cb04e6e2c9cfcdf84765aa0f2389b
Nadiah Elvira Syahputri <nadiahelvira56@gmail.com>
2026-04-21 11:10:28 +0700

    tambah menu sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -751,6 +751,17 @@
             </div>
         </div>
 
+        <div class="col-md-3">
+            <div class="menu-card" style="border:1px solid #aabbcc; background-color:#ffe0ba;">
+                <a href="javascript:addTab('Usulan Penghapusan Harga Komponen', '{{url('ubbrgdw?flagz=KZ')}}')">
+                  <!-- <i class="nav-icon fas fa-anchor icon-blue" style="text-align: center;"></i> -->
+                  <i style="margin-left:-5px;font-size: 40px;" class="nav-icon fas fa-retweet icon-orange"></i>
+                  <h6>Usulan Penghapusan</h6>
+                  <h6>Harga Komponen</h6>
+                </a>
+            </div>
+        </div>
+
       </div>
 
 
